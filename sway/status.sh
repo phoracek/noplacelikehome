c521db84cfea275db52ec79b785a5f28146ff7f9
Petr Horacek <phoracek@redhat.com>
2020-10-14 17:20:50 +0200

    Improve the Sway status bar
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- sway/status.sh
+++ sway/status.sh
@@ -4,14 +4,36 @@
 # You should see changes to the status bar after saving this script.
 # If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
 
-date_formatted=$(date "+%H:%M")
+# Produces "21 days", for example
+uptime_formatted=$(uptime | cut -d ',' -f1 | cut -d ' ' -f4,5 | sed 's/^[[:space:]]*//')
 
+# The ISO-formatted date like 2018-10-06 and the time (e.g., 14:01)
+date_formatted=$(date "+%F %H:%M")
+
+# Returns the battery capacity (%) and status ("Full", "Discharging", or "Charging").
 battery0_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+battery0_status=$(cat /sys/class/power_supply/BAT0/status)
 battery1_capacity=$(cat /sys/class/power_supply/BAT1/capacity)
-sum_battery_capacity=$((battery0_capacity / 2 + battery1_capacity / 2 + 1))
+battery1_status=$(cat /sys/class/power_supply/BAT1/status)
 
+# Storage usage
+storage_total=$(df --output=size -h /dev/dm-2 | sed 1d | sed 's/^[[:space:]]*//')
 storage_used=$(df --output=used -h /dev/dm-2 | sed 1d | sed 's/^[[:space:]]*//')
 
+# Memory usage
+memory_total=$(free -h | awk 'FNR==2{print $2}')
 memory_used=$(free -h | awk 'FNR==2{print $3}')
 
-echo "$date_formatted                                                                                                       $memory_used $storage_used ${sum_battery_capacity}%"
+# Keyboard layout
+layout=$(setxkbmap -query | grep layout | awk '{print $2}')
+
+# Sound stats
+master_status=$(amixer sget Master | grep '\[on\]' &>/dev/null && echo '+' || echo '-')
+master_volume=$(amixer sget Master | grep 'Front Left: ' | sed 's/.*\[\(.*\)\%\].*/\1/')
+capture_status=$(amixer sget Capture | grep '\[on\]' &>/dev/null && echo '+' || echo '-')
+
+# Emojis and characters for the status bar
+# echo "up:[$uptime_formatted] m:[$memory_used/$memory_total] s:[$storage_used/$storage_total] b0:[$battery0_capacity;$battery0_status] b1:[$battery1_capacity;$battery1_status] v:[p$master_volume$master_status;c$capture_status] k:[$layout] t:[$date_formatted]"
+sum_cap=$((battery0_capacity / 2 + battery1_capacity / 2 + 1))
+date_formatted=$(date "+%H:%M")
+echo "$date_formatted                                                                                                       $memory_used $storage_used ${sum_cap}%"
