6f94260f996e48ee4a3d68e80a3fa8b09ae61561
Petr Horacek <phoracek@redhat.com>
2020-10-14 18:00:54 +0200

    Fix battery and storage report for T490s
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- sway/status.sh
+++ sway/status.sh
@@ -9,11 +9,14 @@ battery1_capacity=$(cat /sys/class/power_supply/BAT1/capacity)
 if [ ! -z "${battery1_capacity}" ]; then
     battery_available=$((battery0_capacity / 2 + battery1_capacity / 2 + 1))
 else
-    battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+    battery_available=$(cat /sys/class/power_supply/BAT0/capacity)
 fi
 
 # Storage usage
-storage_available=$(df --output=avail -h /dev/dm-2 | sed 1d | sed 's/^[[:space:]]*//')
+storage_available=$(df --output=avail -h /dev/dm-1 | sed 1d | sed 's/^[[:space:]]*//')
+if [ ! -z ${storage_available} ]; then
+    storage_available=$(df --output=avail -h /dev/dm-2 | sed 1d | sed 's/^[[:space:]]*//')
+fi
 
 # Memory usage
 memory_available=$(free -h | awk 'FNR==2{print $4}')
